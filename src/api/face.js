758767a51866628cbcde2741f3ae46c288a2edad
tokha-tokha <tokha.tokha.ai@gmail.com>
2019-10-30 18:53:11 +1100

    + face recognition (for pop band 'BNK48')

--- src/api/face.js
+++ src/api/face.js
@@ -1,5 +1,7 @@
 import * as faceapi from 'face-api.js';
 
+const maxDescriptorDistance = 0.5;
+
 // Load models and weights
 export async function loadModels() {
     const MODEL_URL = process.env.PUBLIC_URL + '/models';
@@ -29,3 +31,24 @@ export async function getFullFaceDescription(imgBlob, inputSize = 512) {
     
     return fullDesc;    
 } 
+
+export async function createMatcher(faceProfile) {
+    // Create labeled descriptors of member from profile
+    let members = Object.keys(faceProfile);
+    let labeledDescriptors = members.map( 
+        member => new faceapi.LabeledFaceDescriptors(
+            faceProfile[member].name,
+            faceProfile[member].descriptors.map(
+                descriptor => new Float32Array(descriptor)
+            )
+        )
+    );
+
+    // Create face matcher (maximum descriptor distance is 0.5)
+    let faceMatcher = new faceapi.FaceMatcher(
+        labeledDescriptors,
+        maxDescriptorDistance
+    );
+    
+    return faceMatcher
+}
--- src/views/ImageInput.js
+++ src/views/ImageInput.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import { loadModels, getFullFaceDescription } from '../api/face'
+import { loadModels, getFullFaceDescription, createMatcher } from '../api/face'
 
 // Import image to test api
 const testImg = require('../img/test.jpg')
+
+// Import face profile
+const JSON_PROFILE = require('../descriptors/bnk48.json');
 
 // Initial State
 const INIT_STATE = {
     imageURL: testImg,
     fullDesc: null,
-    detections: null
+    detections: null,
+    descriptors: null,
+    match: null
 };
 
 class ImageInput extends Component {
     constructor(props) {
         super(props);
-        this.state = {...INIT_STATE};
+        this.state = { ...INIT_STATE, faceMatcher: null };
     }
 
     componentDidMount = async () => {
         await loadModels();
+        this.setState({ faceMatcher: await createMatcher(JSON_PROFILE) });
         await this.handleImage(this.state.imageURL);
     }
 
@@ -30,10 +36,18 @@ class ImageInput extends Component {
             if (!!fullDesc) {
                 this.setState({
                     fullDesc,
-                    detections: fullDesc.map(fd => fd.detection)
+                    detections: fullDesc.map(fd => fd.detection),
+                    descriptors: fullDesc.map(fd => fd.descriptor)
                 });
             }
         });
+
+        if (!!this.state.descriptors && !! this.state.faceMatcher) {
+            let match = await this.state.descriptors.map(descriptor => 
+                this.state.faceMatcher.findBestMatch(descriptor)
+            );
+            this.setState({ match });
+        }
     }
 
     handleFileChange = async event => {
@@ -50,7 +64,7 @@ class ImageInput extends Component {
     }
 
     render() {
-        const { imageURL, detections } = this.state;
+        const { imageURL, detections, match } = this.state;
 
         let drawBox = null;
         if (!!detections) {
@@ -70,7 +84,23 @@ class ImageInput extends Component {
                                 width: _W,
                                 transform: `translate(${_X}px,${_Y}px)`
                             }}
-                        />
+                        >
+                            {!!match && !!match[i] ? (
+                                <p
+                                    style={{
+                                        backgroundColor: 'blue',
+                                        border: 'solid',
+                                        borderColor: 'blue',
+                                        width: _W,
+                                        marginTop: 0,
+                                        color: '#fff',
+                                        transform: `translate(-3px,${_H}px)`
+                                    }}
+                                >
+                                    {match[i]._label}
+                                </p>
+                            ) : null}
+                        </div>
                     </div>
                 );
 
